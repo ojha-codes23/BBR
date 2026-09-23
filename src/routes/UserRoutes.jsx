c7e2c328cb05f905712cbe491e70da6ab03878b1
Sunil Rathore <yesitlabs.sunil@gmail.com>
2026-09-23 15:23:50 +0530

    change

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -2,19 +2,19 @@ import { Routes, Route } from "react-router-dom";
 
 import UserLayout from "../components/layout/user/UserLayout";
 
-import Dashboard from "../pages/user/Dashboard";
-import Profile from "../pages/user/Profile";
-import Courses from "../pages/user/Courses";
-import Bookings from "../pages/user/Bookings";
+// import Dashboard from "../pages/user/Dashboard";
+// import Profile from "../pages/user/Profile";
+// import Courses from "../pages/user/Courses";
+// import Bookings from "../pages/user/Bookings";
 
 const UserRoutes = () => {
   return (
     <Routes>
       <Route element={<UserLayout />}>
-        <Route path="dashboard" element={<Dashboard />} />
+        {/* <Route path="dashboard" element={<Dashboard />} />
         <Route path="profile" element={<Profile />} />
         <Route path="courses" element={<Courses />} />
-        <Route path="bookings" element={<Bookings />} />
+        <Route path="bookings" element={<Bookings />} /> */}
       </Route>
     </Routes>
   );
